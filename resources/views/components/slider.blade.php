2e1a673ad885ad457d074bcacb5e95fd277751f7
Klenov Vladislav <v.klenov2014@yandex.ru>
2024-07-02 23:38:40 +0500

    Выносит слайдер в отдельный компонент Slider

---
+++ resources/views/components/slider.blade.php
@@ -0,0 +1,30 @@
+@if(!empty($slides))
+    <div class="product_carousel mx-auto">
+        <div id="carouselIndicators" class="carousel slide" data-ride="carousel">
+            <ol class="carousel-indicators">
+                @foreach($slides as $slide)
+                    <li data-target="#carouselIndicators" data-slide-to="{{$slide->id}}"
+                        @class(['active' => $loop->first])
+                    ></li>
+                @endforeach
+                <li data-target="#carouselIndicators" data-slide-to="1"></li>
+                <li data-target="#carouselIndicators" data-slide-to="2"></li>
+            </ol>
+            <div class="carousel-inner">
+                @foreach($slides as $slide)
+                    <div @class(['carousel-item', 'active' => $loop->first])>
+                        <img class="d-block w-100 " src="{{$slides->imagePath}}" alt="First slide">
+                    </div>
+                @endforeach
+            </div>
+            <a class="carousel-control-prev" href="#carouselIndicators" role="button" data-slide="prev">
+                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span class="sr-only">Previous</span>
+            </a>
+            <a class="carousel-control-next" href="#carouselIndicators" role="button" data-slide="next">
+                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                <span class="sr-only">Next</span>
+            </a>
+        </div>
+    </div>
+@endif
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,40 +1,14 @@
-@include ('general.header');
+@include ('general.header')
+
 <main class="upHead">
     <div class="container">
-        <div class="product_carousel mx-auto">
-            <div id="carouselIndicators" class="carousel slide" data-ride="carousel">
-                <ol class="carousel-indicators">
-                    <li data-target="#carouselIndicators" data-slide-to="0" class="active"></li>
-                    <li data-target="#carouselIndicators" data-slide-to="1"></li>
-                    <li data-target="#carouselIndicators" data-slide-to="2"></li>
-                </ol>
-                <div class="carousel-inner">
-                    <div class="carousel-item active">
-                        <img class="d-block w-100 " src="template/img/slide2.jpg" alt="First slide">
-                    </div>
-                    <div class="carousel-item">
-                        <img class="d-block w-100" src="template/img/slide2.jpg" alt="Second slide">
-                    </div>
-                    <div class="carousel-item">
-                        <img class="d-block w-100" src="template/img/slide2.jpg" alt="Third slide">
-                    </div>
-                </div>
-                <a class="carousel-control-prev" href="#carouselIndicators" role="button" data-slide="prev">
-                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                    <span class="sr-only">Previous</span>
-                </a>
-                <a class="carousel-control-next" href="#carouselIndicators" role="button" data-slide="next">
-                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                    <span class="sr-only">Next</span>
-                </a>
-            </div>
-        </div>
+        <x-slider/>
 
-        <div id="cart_carousel" class="cart_carousel">
-            <div class="container">
-                <div class="underline"></div>
-                <div class="wrapper">
-                    <div class="wrapp-hor">
+{{--        <div id="cart_carousel" class="cart_carousel">--}}
+{{--            <div class="container">--}}
+{{--                <div class="underline"></div>--}}
+{{--                <div class="wrapper">--}}
+{{--                    <div class="wrapp-hor">--}}
 {{--                        <?php if (!empty($sliderProducts)) {--}}
 {{--                            foreach ($sliderProducts as $recomend) : ?>--}}
 {{--                                <div class="card">--}}
@@ -60,16 +34,16 @@
 {{--                                </div>--}}
 {{--                            <?php endforeach;--}}
 {{--                        } ?>--}}
-                    </div>
-                    <button id="test-left" class="btn-control btn-control--left"></button>
-                    <button id="test-right" class="btn-control btn-control--right"></button>
-                </div>
-            </div>
-        </div>
+{{--                    </div>--}}
+{{--                    <button id="test-left" class="btn-control btn-control--left"></button>--}}
+{{--                    <button id="test-right" class="btn-control btn-control--right"></button>--}}
+{{--                </div>--}}
+{{--            </div>--}}
+{{--        </div>--}}
 
-        <div class="prod__want">
-            <div class="container">
-                <div class="row">
+{{--        <div class="prod__want">--}}
+{{--            <div class="container">--}}
+{{--                <div class="row">--}}
 {{--                    <? if (!empty($latestProducts)) {--}}
 {{--                        foreach ($latestProducts as $product) : ?>--}}
 {{--                            <div class="col-6 col-sm-6 col-md-4 col-lg-4 col-xl-3 mt-2">--}}
@@ -97,13 +71,13 @@
 {{--                            </div>--}}
 {{--                        <?php endforeach;--}}
 {{--                    } ?>--}}
-                </div>
-            </div>
+{{--                </div>--}}
+{{--            </div>--}}
 
-        </div>
+{{--        </div>--}}
 
-        <div class="center">
-            <div class="grid center" data-masonry='{"itemSelector": ".grid-item", "columnWidth": 76.1, "percentPosition": true}'>
+{{--        <div class="center">--}}
+{{--            <div class="grid center" data-masonry='{"itemSelector": ".grid-item", "columnWidth": 76.1, "percentPosition": true}'>--}}
 {{--                <a href="/category/<? echo $categories[1]['id'] ?>" class="grid-item grid-item--wd2">--}}
 {{--                    <img src="template/img/woman.jpeg" alt="" class="grid-item_img">--}}
 {{--                    <span class="grid-item-name"><? echo $categories[1]['name'] ?></span>--}}
@@ -140,10 +114,10 @@
 {{--                    <img src="template/img/kids.jpg" alt="" class="grid-item_img grid-item--hg2">--}}
 {{--                    <span class="grid-item-name"><? echo $categories[2]['name'] ?></span>--}}
 {{--                </a>--}}
-            </div>
-        </div>
-        <div class="prod__want">
-            <div class="row">
+{{--            </div>--}}
+{{--        </div>--}}
+{{--        <div class="prod__want">--}}
+{{--            <div class="row">--}}
 {{--                <? if !empty($sliderHotProducts) ? foreach ($latestProducts as $product) : ?>--}}
 {{--                <div class="col-6 col-sm-6 col-md-4 col-lg-4 col-xl-3 mt-2">--}}
 {{--                    <div class="card">--}}
@@ -169,14 +143,14 @@
 {{--                    </div>--}}
 {{--                </div>--}}
 {{--                <? endforeach; ?>--}}
-            </div>
-        </div>
-        <div id="hit-prod" class="hit-prod">
-            <div class="container">
-                <h2>Хит продаж</h2>
-                <div class="underline"></div>
-                <div class="wrapper-hit">
-                    <div class="wrapp-hor-hit" data-cycle-fx=carousel data-cycle-timeout=1000 data-cycle-carousel-visible=5 data-cycle-carousel-fluid=true>
+{{--            </div>--}}
+{{--        </div>--}}
+{{--        <div id="hit-prod" class="hit-prod">--}}
+{{--            <div class="container">--}}
+{{--                <h2>Хит продаж</h2>--}}
+{{--                <div class="underline"></div>--}}
+{{--                <div class="wrapper-hit">--}}
+{{--                    <div class="wrapp-hor-hit" data-cycle-fx=carousel data-cycle-timeout=1000 data-cycle-carousel-visible=5 data-cycle-carousel-fluid=true>--}}
 {{--                        <? if !empty($sliderHotProducts) ? foreach ($sliderHotProducts as $hotProduct) : ?>--}}
 {{--                        <div class="card">--}}
 {{--                            <a href="/product/<? echo $hotProduct['id'] ?>"><img src="/template/img/Jilet.jpg" alt="" class="card-img-top"></a>--}}
@@ -200,12 +174,12 @@
 {{--                            </div>--}}
 {{--                        </div>--}}
 {{--                        <? endforeach; ?>--}}
-                    </div>
-                    <button id="left" class="btn-control btn-control--left"></button>
-                    <button id="right" class="btn-control btn-control--right"></button>
-                </div>
-            </div>
-        </div>
+{{--                    </div>--}}
+{{--                    <button id="left" class="btn-control btn-control--left"></button>--}}
+{{--                    <button id="right" class="btn-control btn-control--right"></button>--}}
+{{--                </div>--}}
+{{--            </div>--}}
+{{--        </div>--}}
     </div>
 </main>
-@include ('general.footer');
+@include ('general.footer')
