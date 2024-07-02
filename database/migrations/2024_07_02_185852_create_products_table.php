96b6a97589caea0834964355e6879b1290b2a729
Klenov Vladislav <v.klenov2014@yandex.ru>
2024-07-03 00:50:53 +0500

    Добавляет миграции таблиц продуктов, категорий и связей между ними

---
+++ database/migrations/2024_07_02_185852_create_products_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id()->autoIncrement();
+            $table->string('name')->nullable(false);
+            $table->boolean('active')->default(false);
+            $table->float('price')->default(0);
+            $table->integer('quantity')->default(0);
+            $table->string('description')->nullable();
+            $table->string('picture_path')->nullable();
+            $table->integer('category_id')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('products');
+    }
+};
