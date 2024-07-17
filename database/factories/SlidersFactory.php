42a09ca8783865ac80b0375f9b7944102f529319
Klenov Vladislav <v.klenov2014@yandex.ru>
2024-07-18 00:26:27 +0500

    Добавляет таблицу с рекламными слайдерами

---
+++ database/factories/SlidersFactory.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Sliders>
+ */
+class SlidersFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            "name" => fake()->name(),
+            "active" => fake()->boolean,
+            "picture_path" => fake()->imageUrl(1920, 1080, 'berries'),
+            "description" => fake()->text(),
+            "sort_order" => fake()->randomDigit(),
+        ];
+    }
+}
