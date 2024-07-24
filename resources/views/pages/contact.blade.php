ad17d9aa3718124532459619e27641f4575bf593
Vlad Klenov <v.klenov2014@yandex.ru>
2024-07-25 00:47:57 +0500

    Добавляет вывод ошибок и информации об успешной обработке формы на странице контактов

--- resources/views/pages/contact.blade.php
+++ resources/views/pages/contact.blade.php
@@ -4,27 +4,27 @@
         <div class="container">
             <div class="row">
                 <div class="form-control">
-                    @if ($errors->any())
-{{--                    <h2>Сообщение отправлено!</h2>--}}
-{{--                    <h4>Мы ответим вам на указанный вами E-mail.</h4>--}}
-{{--                    <? else : ?>--}}
-{{--                        <?php if (isset($errors) && is_array($errors)) : ?>--}}
-                    <ul>
-                        @foreach ($errors->all() as $error)
-                            <li> - {{ $error }}</li>
-                        @endforeach
-                    </ul>
+                    @if($status === "OK")
+                        <h2>Сообщение отправлено!</h2>
+                        <h4>Мы ответим вам на указанный вами E-mail.</h4>
+                    @else
+                        @if (!$errors->isEmpty())
+                            <ul>
+                                @foreach ($errors->all() as $error)
+                                    <li> - {{ $error }}</li>
+                                @endforeach
+                            </ul>
+                        @endif
+                        <form action="/contact" class="sign" method="POST">
+                            @csrf
+                            <h3>Обратная свзяь</h3>
+                            <label for="">Введите ваш E-mail</label>
+                            <input type="email" name="email" class="form-control" placeholder="E-mail" value="{{ old('email') }}">
+                            <label for="">Ваше сообщение</label>
+                            <textarea type="text" name="message" rows="5" class="form-control">{{ old('message') }}</textarea>
+                            <button class="sign_button mt-4" name="submit" type="submit">Отправить</button>
+                        </form>
                     @endif
-                    <form action="#" class="sign" method="POST">
-                        @csrf
-                        <h3>Обратная свзяь</h3>
-                        <label for="">Введите ваш E-mail</label>
-                        <input type="email" name="userEmail" class="form-control" placeholder="E-mail" value="{{ old('userEmail') }}">
-                        <label for="">Ваше сообщение</label>
-                        <textarea type="text" name="userText" rows="5" class="form-control" value="{{ old('userText') }}"></textarea>
-                        <button class="sign_button mt-4" name="submit" type="submit">Отправить</button>
-                    </form>
-{{--                    <?// endif; ?> --}}
                 </div>
             </div>
         </div>
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\FormController;
+use App\Http\Controllers\FormHandlerController;
 use App\Http\Controllers\ProductsController;
 use Illuminate\Support\Facades\Route;
 
@@ -15,19 +17,17 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
+Route::get('/', static function () {
     return view('pages.index');
 });
 
-Route::get('/contact', function () {
-    return view('pages.contact');
-});
+Route::any('/contact', [FormController::class, 'contact'])->name('contact');
 
-Route::get('/login', function () {
+Route::get('/login', static function () {
     return view('pages.sign-in');
 });
 
-Route::get('/register', function () {
+Route::get('/register', static function () {
    return view('pages.sign-up');
 });
 
@@ -43,7 +43,9 @@ Route::controller(CategoryController::class)->prefix('catalog')->group(
     }
 );
 
-Route::get('/basket', function () {
+//Route::post('/send/question', [FormController::class, 'handle']);
+
+Route::get('/basket', static function () {
     return view('pages.basket');
 });
 
