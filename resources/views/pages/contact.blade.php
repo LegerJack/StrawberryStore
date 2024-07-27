eae55a92fcb0acdee0a9c0387e9f8ce0a5072146
Vlad Klenov <v.klenov2014@yandex.ru>
2024-07-27 23:03:36 +0500

    Изменяет вывод ошибок для формы вопросов на странице контактов

--- resources/views/pages/contact.blade.php
+++ resources/views/pages/contact.blade.php
@@ -1,34 +1,33 @@
 @include ('general.header')
-<main class="upHead">
     <div class="prod__want">
         <div class="container">
             <div class="row">
-                <div class="form-control">
-                    @if($status === "OK")
-                        <h2>Сообщение отправлено!</h2>
-                        <h4>Мы ответим вам на указанный вами E-mail.</h4>
-                    @else
-                        @if (!$errors->isEmpty())
-                            <ul>
-                                @foreach ($errors->all() as $error)
-                                    <li> - {{ $error }}</li>
-                                @endforeach
-                            </ul>
-                        @endif
-                        <form action="/contact" class="sign" method="POST">
-                            @csrf
-                            <h3>Обратная свзяь</h3>
-                            <label for="">Введите ваш E-mail</label>
-                            <input type="email" name="email" class="form-control" placeholder="E-mail" value="{{ old('email') }}">
-                            <label for="">Ваше сообщение</label>
-                            <textarea type="text" name="message" rows="5" class="form-control">{{ old('message') }}</textarea>
-                            <button class="sign_button mt-4" name="submit" type="submit">Отправить</button>
-                        </form>
-                    @endif
-                </div>
+                @if(!empty($status) && $status === "OK")
+                    <h2>Сообщение отправлено!</h2>
+                    <h4>Мы ответим вам на указанный вами E-mail.</h4>
+                @else
+                    <form action="/contact" class="sign" method="POST">
+                        @csrf
+                        <h3>Обратная свзяь</h3>
+                        <label for="email" class="form-label">Введите ваш E-mail</label>
+                        <input type="email" name="email" id="email" class="form-control" placeholder="E-mail" value="{{ old('email') }}">
+                        @error('email')
+                            <div class="form-text">
+                                Неверный e-mail
+                            </div>
+                        @enderror
+                        <label for="message" class="form-label">Ваше сообщение</label>
+                        <textarea type="text" name="message" id="message" rows="5" class="form-control">{{ old('message') }}</textarea>
+                        @error('message')
+                        <div class="form-text">
+                            Ошибка обработки сообщения
+                        </div>
+                        @enderror
+                        <button class="sign_button mt-4" name="submit" type="submit">Отправить</button>
+                    </form>
+                @endif
             </div>
         </div>
     </div>
 
-</main>
 @include ('general.footer')
