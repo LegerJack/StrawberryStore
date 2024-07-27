c7b90799b94e242b4eaa5c4bcc06dde2f257376b
Vlad Klenov <v.klenov2014@yandex.ru>
2024-07-28 00:23:09 +0500

    Добавляет подключение хлебных крошек на страницу просмотра товаров категории

--- resources/views/web/sections/catalog/category.blade.php
+++ resources/views/web/sections/catalog/category.blade.php
@@ -1,6 +1,7 @@
 @extends('web.app')
 @section('content')
     <div class="container">
+        {{ Breadcrumbs::render('category', $category) }}
         <h2>{{ $category->name }}</h2>
         <x-block.products-list :products="$products"/>
     </div>
